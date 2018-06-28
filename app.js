f61110be9f9febf07665759354f8c6e1b29d9ba7
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-06-28 13:48:20 -0600

    added requst.post

--- app.js
+++ app.js
@@ -34,29 +34,29 @@ app.use('/users', users);
 app.use('/vaults', vaults);
 //test
 app.post('/auth/github', (req, res, next) => {
-  console.log("req",req)
-  console.log("body", req.body)
+  // console.log("req",req)
+  // console.log("body", req.body)
   console.log("code", req.body.code)
-  // const code = req.params.code
-  // if (!code) {
-  //   return next()
-  // }
-  // request.post('https://github.com/login/oauth/access_token', {
-  //   headers: {
-  //     "accept": "application/json"
-  //   },
-  //   form: {
-  //     code,
-  //     client_id: process.env.GITHUB_CLIENT_ID,
-  //     client_secret: process.env.GITHUB_CLIENT_SECRET
-  //   }
-  // }, (err, response, body) => {
-  //   if (err) return next(err)
-  //   console.log('RESPONSE', response)
-  //   console.log('BODY', body)
-  //   const github = JSON.parse(body)
-  //   res.json({testKey: true, accessToken: github.access_token})
-  // })
+  const code = req.body.code
+  if (!code) {
+    return next()
+  }
+  request.post('https://github.com/login/oauth/access_token', {
+    headers: {
+      "accept": "application/json"
+    },
+    form: {
+      code,
+      client_id: process.env.GITHUB_CLIENT_ID,
+      client_secret: process.env.GITHUB_CLIENT_SECRET
+    }
+  }, (err, response, body) => {
+    if (err) return next(err)
+    console.log('RESPONSE', response)
+    console.log('BODY', body)
+    const github = JSON.parse(body)
+    res.json({testKey: true, accessToken: github.access_token})
+  })
 })
 //test
 app.get('/auth/github/:code', (req, res, next) => {
