2983842c4ca3449871f6d36e536682743e67a276
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-06-28 20:11:50 -0600

    added auth

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const vaults = require('./api/vaults.js')
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type','Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   // if (req.method === "OPTIONS") 
   //       res.send(200);
   //   else (next())
