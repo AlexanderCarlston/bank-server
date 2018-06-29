901ba27b17bd4d60157bfd157098033b55b9d8e5
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-06-28 19:12:49 -0600

    srew u options

--- app.js
+++ app.js
@@ -19,10 +19,9 @@ var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Content-Type');
-  // if (req.method === "OPTIONS") 
-  //       res.send(200);
-  //   else (next())
-    next()
+  if (req.method === "OPTIONS") 
+        res.send(200);
+    else (next())
 }
 app.use(allowCrossDomain)
 
