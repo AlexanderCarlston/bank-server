a217131e8acfcd84218d8c475dc327513a7c3b59
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-06-28 18:37:46 -0600

    stupid header

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ const users = require('./api/users.js')
 const vaults = require('./api/vaults.js')
 
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
   if (req.method === "OPTIONS") 
         res.send(200);
     else (next())
