7508681cf10226857ac075021fdd1b7fdfe325e7
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-06-28 19:00:59 -0600

    added to allowcrossdomain

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const vaults = require('./api/vaults.js')
 
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === "OPTIONS") 
         res.send(200);
