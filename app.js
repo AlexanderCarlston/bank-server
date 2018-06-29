4a1b0fa315a47db3d725880ab7026a7b35c777bf
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-06-28 19:37:50 -0600

    added auth

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const vaults = require('./api/vaults.js')
 
 var allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS,Authorization')
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === "OPTIONS") 
         res.send(200);
@@ -34,12 +34,12 @@ app.use(cookieParser());
 app.use('/users', users);
 app.use('/vaults', vaults);
 //test
-app.options("/*", function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.send(200);
-});
+// app.options("/*", function(req, res, next){
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//   res.send(200);
+// });
 app.post('/auth/github', (req, res, next) => {
   const code = req.body.code
   if (!code) {
